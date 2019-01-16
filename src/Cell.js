4aa18067a860fc2b2e177ce1f8c76a7369d0d0e3
StephenAI <stephenaigbomian@gmail.com>
2019-01-16 01:05:04 -0600

    removed images

--- src/Cell.js
+++ src/Cell.js
@@ -32,8 +32,9 @@ class Cell extends Component{
         let file_url = `https://github.com/Stephen-AI/React-Chess/raw/master/src/${pref}${this.props.piece}.png`
         return (
             <div className="cell" style={styl} onClick={this.props.onClick}
-                width="42" height="42">
-                <img src={file_url} alt={this.props.piece}></img>
+                >
+                <img src={file_url} alt={this.props.piece}
+                    width="42" height="42"></img>
             </div>
         )
     }
